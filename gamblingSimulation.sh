102b6981e2682997760f64d83864670b5bf8c783
Admin <none@gmail.com>
2020-03-07 13:56:46 +0530

    Added: Win and Lose By How Much

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -5,28 +5,46 @@
 # @Version: 18.04.3 lts
 # @Since: 07 March 2020 / Saturday
 
-stakes=100
+stakesAmount=100
 bet=1
 playingGoal=150
 resignLimit=50
 totalAmount=0
+monthDays=20
+
+function playForMonth()
+{
+	echo Playing For Month
+	expectedGoalForMonth=$(($stakesAmount*$monthDays))
+	for (( i=1; i<=$monthDays; i++ ))
+	do
+		play
+		totalAmount=$(($totalAmount+$stakesAmount))
+	done
+	if [[ $totalAmount -lt $expectedGoalForMonth ]]
+	then
+		echo "Gambler Lose by $(($expectedGoalForMonth-$totalAmount))$"
+	elif [[ $totalAmount -gt $expectedGoalForMonth ]]
+	then
+		echo "Gambler Won by $(($totalAmount-$expectedGoalForMonth))$"
+	else
+		echo Gambler Achieved Goal
+	fi
+}
 
 function play()
 {
-	for (( i=1; i<=20; i++ ))
-	do
-		stakes=100
-		while [[ $stakes -gt $resignLimit && $stakes -lt $playingGoal ]]
-		do
-			if [[ $((RANDOM%2)) -eq 1 ]]
-			then
-				stakes=$(($stakes+$bet))
-			else
-				stakes=$(($stakes-$bet))
-			fi
-		done
-		totalAmount=$(($totalAmount+$stakes))
-	done
-	echo $totalAmount
+	stakesAmount=100
+      while [[ $stakesAmount -gt $resignLimit && $stakesAmount -lt $playingGoal ]]
+      do
+         if [[ $((RANDOM%2)) -eq 1 ]]
+         then
+            stakesAmount=$(($stakesAmount+$bet))
+         else
+            stakesAmount=$(($stakesAmount-$bet))
+         fi
+      done
+#	echo $stakesAmount
 }
-play
+
+playForMonth

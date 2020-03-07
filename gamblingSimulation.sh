b98a4152b92c94c04e3d6a6f5804a6fa0c0d4ee0
Admin <none@gmail.com>
2020-03-07 11:37:30 +0530

    Added: Gambler Win Or Lose

---
+++ gamblingSimulation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+# @Description: Gambling Simulation Problem
+# @Author: Akshay Dhananjay Barve
+# @Version: 18.04.3 lts
+# @Since: 07 March 2020 / Saturday
+
+stakes=100
+bet=1
+
+if [[ $((RANDOM%2)) -eq 1 ]]
+then
+	stakes=$(($stakes+$bet))
+else
+	stakes=$(($stakes-$bet))
+fi

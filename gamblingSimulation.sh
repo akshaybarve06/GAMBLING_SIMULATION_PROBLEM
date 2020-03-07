5192bdd9645b6ceded7bacda426384fc1b77cfbb
Admin <none@gmail.com>
2020-03-07 15:14:17 +0530

    Added: Find Luckiest and UnLuckiest Day

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -12,39 +12,70 @@ resignLimit=50
 totalAmount=0
 monthDays=20
 
+declare -A winCount
+declare -A loseCount
+
 function playForMonth()
 {
-	echo Playing For Month
+	echo "Playing For Month.."
 	expectedGoalForMonth=$(($stakesAmount*$monthDays))
+
 	for (( i=1; i<=$monthDays; i++ ))
 	do
-		play
+		stakesAmount=100
+		while [[ $stakesAmount -gt $resignLimit && $stakesAmount -lt $playingGoal ]]
+		do
+			if [[ $((RANDOM%2)) -eq 1 ]]
+		 	then
+			    	stakesAmount=$(($stakesAmount+$bet))
+					winCount[$i]=$((${winCount[$i]}+1))
+		 	else
+			    	stakesAmount=$(($stakesAmount-$bet))
+					loseCount[$i]=$((${loseCount[$i]}+1))
+		 	fi
+		done
 		totalAmount=$(($totalAmount+$stakesAmount))
 	done
-	if [[ $totalAmount -lt $expectedGoalForMonth ]]
-	then
-		echo "Gambler Lose by $(($expectedGoalForMonth-$totalAmount))$"
-	elif [[ $totalAmount -gt $expectedGoalForMonth ]]
-	then
-		echo "Gambler Won by $(($totalAmount-$expectedGoalForMonth))$"
-	else
-		echo Gambler Achieved Goal
-	fi
+
+		if [[ $totalAmount -lt $expectedGoalForMonth ]]
+		then
+				echo "Gambler Lose by $(($expectedGoalForMonth-$totalAmount))$"
+				unluckyDay
+		elif [[ $totalAmount -gt $expectedGoalForMonth ]]
+		then
+				echo "Gambler Won by $(($totalAmount-$expectedGoalForMonth))$"
+				luckyDay
+		else
+				echo "Gambler Neither Won or Loose Neutral Achieved Goal"
+		fi
 }
 
-function play()
+function luckyDay()
 {
-	stakesAmount=100
-      while [[ $stakesAmount -gt $resignLimit && $stakesAmount -lt $playingGoal ]]
-      do
-         if [[ $((RANDOM%2)) -eq 1 ]]
-         then
-            stakesAmount=$(($stakesAmount+$bet))
-         else
-            stakesAmount=$(($stakesAmount-$bet))
-         fi
-      done
-#	echo $stakesAmount
+	winnings=${winCount["1"]}
+	for k in ${!winCount[@]}
+	do
+		if [[ $winnings -lt ${winCount["$k"]} ]]
+		then
+			winnings=${winCount["$k"]}
+			luckyDay=$k
+		fi
+	done
+	echo "Lucky Day is $luckyDay & no. of Winnings $winnings"
+}
+
+function unluckyDay()
+{
+	losings=${loseCount["1"]}
+	for k in ${!loseCount[@]}
+	do
+		if [[ $losings -lt ${loseCount["$k"]} ]]
+		then
+			losings=${loseCount["$k"]}
+			unluckyDay=$k
+		fi
+	done
+	echo "Unlucky Day is $unluckyDay & no. of losings $losings"
 }
 
 playForMonth
